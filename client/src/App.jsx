00b94dc20140ada680800e68af64efe066d95922
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-07-22 15:44:56 +0530

    file updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar.jsx';
 import {Route,Routes} from 'react-router-dom  '; // Assuming you have a Routes component for routing
 import Home from './pages/Home';
+import Movies from './pages/Movies';
 
 const App = () => {
   return (
@@ -9,6 +10,10 @@ const App = () => {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path = "/movies" element={<Movies />} />
+        <Route path = "/movies/:id" element={<MoviesDetails />} />
+        <Route path = "/movies/:id/:date" element={<MoviesDetails />} />
+
       </Routes>
     </>
   )
