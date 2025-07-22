44a1a1ac5193c202d477b7aa64ec44d4a847da47
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-07-22 15:41:43 +0530

    file updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Navbar from './components/Navbar.jsx';
+import {Route,Routes} from 'react-router-dom  '; // Assuming you have a Routes component for routing
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  );
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
