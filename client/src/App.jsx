8cbda76e052e309ebdd64791f937d478a16fab0c
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-07-22 15:48:12 +0530

    file updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from './components/Navbar.jsx';
 import {Route,Routes} from 'react-router-dom  '; // Assuming you have a Routes component for routing
 import Home from './pages/Home';
 import Movies from './pages/Movies';
+import SeatALayout from './pages/SeatALayout';
+import MovieDetails from './pages/MovieDetails';
+import MyBooking from './pages/MyBooking';
+import Favorites from './pages/Favorites';
 
 const App = () => {
   return (
@@ -11,8 +15,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path = "/movies" element={<Movies />} />
-        <Route path = "/movies/:id" element={<MoviesDetails />} />
-        <Route path = "/movies/:id/:date" element={<MoviesDetails />} />
+        <Route path = "/movies/:id" element={<MovieDetails />} />
+        <Route path = "/movies/:id/:date" element={<SeatALayout />} />
+        <Route path = "/mybookings" element={<MyBooking />} />
+        <Route path = "/favorites" element={<Favorites />} />
+
+
 
       </Routes>
     </>
