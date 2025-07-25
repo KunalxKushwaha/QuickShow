b3e851cd081e6ccbc7c0973bef498a8140f5b76d
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-07-25 12:07:10 +0530

    file updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import {Route,Routes} from 'react-router-dom  '; // Assuming you have a Routes component for routing
+import { Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import Movies from './pages/Movies';
 import SeatALayout from './pages/SeatALayout';
